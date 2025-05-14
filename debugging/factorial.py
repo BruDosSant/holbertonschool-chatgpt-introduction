06a2d080af236dd9c7b3b67ee2048fa93ce5303f
root <root@PCera.>
2025-05-14 09:58:17 -0300

    hola pa

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,21 +5,8 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # Decrementar n para evitar bucle infinito meesmo
+        n -= 1  # Faltaba esta línea
     return result
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: {} <number>".format(sys.argv[0]))
-        sys.exit(1)
-
-    try:
-        num = int(sys.argv[1])
-        if num < 0:
-            raise ValueError("Number must be non-negative")
-    except ValueError as e:
-        print("Error:", e)
-        sys.exit(1)
-
-    f = factorial(num)
-    print(f)
+f = factorial(int(sys.argv[1]))
+print(f)
